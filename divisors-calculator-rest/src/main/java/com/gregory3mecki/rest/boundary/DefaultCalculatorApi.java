caca4fa06d6a4e316d8e1d52630ce2665a69b12f
Grzegorz Szymecki <gregory.3mecki@gmail.com>
2023-03-04 23:54:16 +0100

    Basic version of calculator-api.
    * calculator service prepared

---
+++ divisors-calculator-rest/src/main/java/com/gregory3mecki/rest/boundary/DefaultCalculatorApi.java
@@ -0,0 +1,31 @@
+package com.gregory3mecki.rest.boundary;
+
+import com.gregory3mecki.rest.api.boundary.CalculatorApi;
+import com.gregory3mecki.rest.control.CalculatorService;
+
+import javax.inject.Inject;
+import javax.ws.rs.core.Response;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class DefaultCalculatorApi implements CalculatorApi {
+
+    @Inject
+    CalculatorService calculatorService;
+
+    @Override
+    public Response provideMappedDividers(final String mapperName, final List<String> numbers) {
+        final Collection<Integer> numbersToProcess = prepareNumbers(numbers);
+        final Map<Integer, Collection<String>> result = calculatorService.provideMappedDividers(mapperName, numbersToProcess);
+        return Response.ok(result).build();
+    }
+
+    private Collection<Integer> prepareNumbers(final Collection<String> numbers) {
+        return numbers.stream()
+                .map(Integer::valueOf)
+                .collect(Collectors.toList());
+    }
+
+}
