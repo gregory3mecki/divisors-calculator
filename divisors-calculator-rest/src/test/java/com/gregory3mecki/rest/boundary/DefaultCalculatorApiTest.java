df192a8f851e66c3dbdc8c82e65beac7faeb33c8
Grzegorz Szymecki <gregory.3mecki@gmail.com>
2023-03-05 13:14:50 +0100

    Calculator-api tests.
    * calculator-api structure modified
    * new tests created

---
+++ divisors-calculator-rest/src/test/java/com/gregory3mecki/rest/boundary/DefaultCalculatorApiTest.java
@@ -0,0 +1,25 @@
+package com.gregory3mecki.rest.boundary;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+class DefaultCalculatorApiTest {
+
+    private static final String result = "{\"4\":[\"testValue01\",\"testValue02\",\"testValue04\"]}";
+
+    @Test
+    void checkCalculatorReturnsCorrectDivisorsForSimpleCase() {
+        given()
+                .pathParam("name", "test-mapping")
+                .queryParam("number", "4")
+                .when().get("/calculator-api/dividers/mappings/{name}")
+                .then()
+                .statusCode(200)
+                .body(is(result));
+    }
+
+}
