565d5b25b04b4b75c09a0528777f7402869ae3c6
Grzegorz Szymecki <gregory.3mecki@gmail.com>
2023-03-10 22:25:08 +0100

    Positive number validation.
    * input numbers validation created

--- divisors-calculator-rest/src/main/java/com/gregory3mecki/rest/boundary/DefaultCalculatorApi.java
+++ divisors-calculator-rest/src/main/java/com/gregory3mecki/rest/boundary/DefaultCalculatorApi.java
@@ -1,5 +1,6 @@
 package com.gregory3mecki.rest.boundary;
 
+import com.gregory3mecki.core.util.exception.StatusCodeException;
 import com.gregory3mecki.core.util.rest.RestExceptionHandler;
 import com.gregory3mecki.rest.api.boundary.CalculatorApi;
 import com.gregory3mecki.rest.api.entity.ResponseDTO;
@@ -9,7 +10,6 @@ import javax.inject.Inject;
 import javax.ws.rs.core.Response;
 import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class DefaultCalculatorApi implements CalculatorApi {
@@ -20,9 +20,9 @@ public class DefaultCalculatorApi implements CalculatorApi {
     @RestExceptionHandler
     @Override
     public Response provideMappedDividers(final String mappingName, final List<String> numbers) {
-        final Collection<Integer> numbersToProcess = prepareNumbers(numbers);
-        final Map<Integer, Collection<String>> result = calculatorService.provideMappedDividers(mappingName, numbersToProcess);
-        final ResponseDTO response = new ResponseDTO();
+        final var numbersToProcess = prepareNumbers(numbers);
+        final var result = calculatorService.provideMappedDividers(mappingName, numbersToProcess);
+        final var response = new ResponseDTO();
         response.setData(result);
         return Response
                 .ok(response)
@@ -31,8 +31,21 @@ public class DefaultCalculatorApi implements CalculatorApi {
 
     private Collection<Integer> prepareNumbers(final Collection<String> numbers) {
         return numbers.stream()
+                .peek(this::checkIsPositiveNumber)
                 .map(Integer::valueOf)
                 .collect(Collectors.toList());
+    }
+
+    private void checkIsPositiveNumber(final String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException | NullPointerException exception) {
+            throw StatusCodeException.builder()
+                    .statusCode(406)
+                    .exceptionMessage("Input value should be positive number.")
+                    .details(String.format("Invalid input value %s.", input))
+                    .build();
+        }
     }
 
 }
