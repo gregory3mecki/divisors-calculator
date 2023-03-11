d33c90d31b512884335c2b33ee36ab048ef89194
Grzegorz Szymecki <gregory.3mecki@gmail.com>
2023-03-11 15:50:27 +0100

    Logging framework.
    * logging framework used and logging format updated

--- divisors-calculator-rest/src/main/java/com/gregory3mecki/rest/boundary/DefaultCalculatorApi.java
+++ divisors-calculator-rest/src/main/java/com/gregory3mecki/rest/boundary/DefaultCalculatorApi.java
@@ -1,10 +1,12 @@
 package com.gregory3mecki.rest.boundary;
 
+import com.google.common.base.Preconditions;
 import com.gregory3mecki.core.util.exception.StatusCodeException;
 import com.gregory3mecki.core.util.rest.RestExceptionHandler;
 import com.gregory3mecki.rest.api.boundary.CalculatorApi;
 import com.gregory3mecki.rest.api.entity.ResponseDTO;
 import com.gregory3mecki.rest.control.CalculatorService;
+import org.jboss.logging.Logger;
 
 import javax.inject.Inject;
 import javax.ws.rs.core.Response;
@@ -15,11 +17,15 @@ import java.util.stream.Collectors;
 public class DefaultCalculatorApi implements CalculatorApi {
 
     @Inject
+    Logger logger;
+
+    @Inject
     CalculatorService calculatorService;
 
     @RestExceptionHandler
     @Override
     public Response provideMappedDividers(final String mappingName, final List<String> numbers) {
+        logger.infof("Calculating divisors for given numbers %s.", numbers);
         final var numbersToProcess = prepareNumbers(numbers);
         final var result = calculatorService.provideMappedDividers(mappingName, numbersToProcess);
         final var response = new ResponseDTO();
@@ -38,12 +44,15 @@ public class DefaultCalculatorApi implements CalculatorApi {
 
     private void checkIsPositiveNumber(final String input) {
         try {
-            Integer.parseInt(input);
-        } catch (NumberFormatException | NullPointerException exception) {
+            final int number = Integer.parseInt(input);
+            Preconditions.checkArgument(number >= 0);
+        } catch (IllegalArgumentException | NullPointerException exception) {
+            final String message = "Invalid input value %s.";
+            logger.errorf(exception, message, input);
             throw StatusCodeException.builder()
                     .statusCode(406)
                     .exceptionMessage("Input value should be positive number.")
-                    .details(String.format("Invalid input value %s.", input))
+                    .details(String.format(message, input))
                     .build();
         }
     }
